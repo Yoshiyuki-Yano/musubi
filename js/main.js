491ed4d1400e2320ad86fc3280a3ff559656fa42
Yoshiyuki Yano <70365451+Yoshiyuki-Yano@users.noreply.github.com>
2021-01-15 13:08:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@
   const About = document.getElementById('overlay-About');
   const ClassRoom = document.getElementById('overlay-ClassRoom');
   const Price = document.getElementById('overlay-Price');
+  const Carender = document.getElementById('overlay-Carender');
   const Form = document.getElementById('overlay-Form');
 
   function overlayClose(link) {
@@ -31,8 +32,8 @@
   overlayClose(About);
   overlayClose(ClassRoom);
   overlayClose(Price);
+  overlayClose(Carender);
   overlayClose(Form);
-
   
   const menuItems = document.querySelectorAll('.Course-Menu li a');
   const contents = document.querySelectorAll('.Content');
@@ -52,4 +53,132 @@
       document.getElementById(clickedItem.dataset.id).classList.add('Active');
     });
   });
+
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    return dates;
+  }
+
+  function getCalendarBody() {
+    const dates = []; 
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+    }
+
+    if (year === today.getFullYear() && month === today.getMonth()) {
+      dates[today.getDate() - 1].isToday = true;
+    }
+    return dates;
+  }
+
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    return dates;
+  }
+  function clearCalendar() {
+    const tbody = document.querySelector('tbody');
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+  }
+
+  function renderTitle() {
+    const title = `${year}/${String(month + 1).padStart(2, '0')}`;
+    document.getElementById('title').textContent = title;
+  }
+
+  function renderWeeks() {
+    const dates = [
+      ...getCalendarHead(),
+      ...getCalendarBody(),
+      ...getCalendarTail(),
+    ];
+    const weeks = [];
+    const weeksCount = dates.length / 7;
+
+    for (let i = 0; i < weeksCount; i++) {
+      weeks.push(dates.splice(0, 7));
+    }
+
+    weeks.forEach(week => {
+      const tr = document.createElement('tr');
+      week.forEach(date => {
+        const td = document.createElement('td');
+
+        td.textContent = date.date;
+        if (date.isToday) {
+          td.classList.add('today');
+        }
+        if (date.isDisabled) {
+          td.classList.add('disabled');
+        }
+
+        tr.appendChild(td);
+      });
+      document.querySelector('tbody').appendChild(tr);
+    });
+  }
+
+  function createCalendar() {
+    clearCalendar();
+    renderTitle();
+    renderWeeks();
+  }
+
+  document.getElementById('prev').addEventListener('click', () => {
+    month--;
+    if (month < 0) {
+      year--;
+      month = 11;
+    }
+
+    createCalendar();
+  });
+
+  document.getElementById('next').addEventListener('click', () => {
+    month++;
+    if (month > 11) {
+      year++;
+      month = 0;
+    }
+
+    createCalendar();
+  });
+
+  document.getElementById('today').addEventListener('click', () => {
+    year = today.getFullYear();
+    month = today.getMonth();
+
+    createCalendar();
+  });
+
+  createCalendar();
 }
